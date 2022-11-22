b87a9d7ee364bb747bfa43643d6e87676e176205
Raj Panjwani <rajpanjwani20@gmail.com>
2022-11-21 21:09:07 -0500

    first commit

---
+++ data/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import *
+
+# Register your models here.
+admin.site.register(Education)
+admin.site.register(Certification)
+admin.site.register(Project)
+admin.site.register(Company)
